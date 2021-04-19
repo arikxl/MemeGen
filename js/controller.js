9521f884f9d10f67c99b478219000b03a384efa8
Arik Alexandrov <arikxl@gmail.com>
2021-04-19 22:27:29 +0300

    search&filter

--- js/controller.js
+++ js/controller.js
@@ -4,3 +4,27 @@ function onInit() {
     console.log('LETS MEME!');
 
 }
+
+
+
+function openFilters() {
+    document.querySelector('.dropdown-content').classList.toggle('show');
+}
+
+
+// צריך להחליף לפונקציית פילטר נורמלית
+function filterMemes() {
+    var filter, a,  txtValue;
+    var filterInput = document.querySelector('.filter-input');
+    filter = filterInput.value.toUpperCase();
+    var filterDropdown = document.querySelector('.dropdown-content');
+    a = filterDropdown.getElementsByTagName("a");
+    for (var i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
